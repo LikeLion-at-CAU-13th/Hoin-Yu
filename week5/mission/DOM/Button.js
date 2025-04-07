eb14ee7c20574799b7da2693e6416eecde586447
hoinhoin <follow3448@naver.com>
2025-04-08 00:53:58 +0900

    코드 정리

--- week5/mission/DOM/Button.js
+++ week5/mission/DOM/Button.js
@@ -1,12 +1,3 @@
-// import DOM from "./DOM.js";
-
-// class Button extends DOM {
-//     constructor(innerText, className) {
-//         super('button', innerText, className);
-//     }
-// }
-// export default Button;
-
 import DOM from "./DOM.js";
 
 class Button extends DOM {
--- week5/mission/controller/CompleteController.js
+++ week5/mission/controller/CompleteController.js
@@ -1,15 +1,17 @@
 import Div from "../DOM/Div.js";
 import Button from "../DOM/Button.js";
-
+import Complete from "../DOM/Complete.js";
 class CompleteController {
     constructor(todoText) {
-        // 완료된 작업을 위한 row 생성
+        this.newTodo = new Complete(todoText);
+
+
+        this.todoText = todoText
         this.row = document.createElement('div');
         this.row.classList.add('row');
         
-        // 텍스트 박스 생성
         this.innerText = new Div(todoText, 'text-box').node;
-        this.innerText.classList.add('done-text'); // 취소선 스타일 유지
+        //this.innerText.classList.add('done-text');
         
         // 삭제 버튼 생성
         this.delBtn = new Button('삭제', 'del-btn').node;
@@ -19,7 +21,6 @@ class CompleteController {
             this.delComplete();
         });
         
-        // row에 요소들 추가
         this.row.appendChild(this.innerText);
         this.row.appendChild(this.delBtn);
     }
